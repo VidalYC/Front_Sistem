14e8819b00933a68be75f8d69d0edf61cf63fe20
Yoriel <yorielvidal@gmail.com>
2024-10-15 13:58:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ChartPage from './pages/ChartPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />  {/* Barra de navegación para todas las páginas */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />  {/* Ruta para el Home */}
+        <Route path="/login" element={<LoginPage />} />  {/* Ruta para Login */}
+        <Route path="/register" element={<RegisterPage />} />  {/* Ruta para Registro */}
+        <Route path="/chart" element={<ChartPage />} />  {/* Ruta para la gráfica interactiva */}
+      </Routes>
+    </Router>
   );
 }
 
